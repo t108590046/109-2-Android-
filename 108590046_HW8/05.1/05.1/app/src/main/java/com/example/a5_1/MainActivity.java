d251fcd8267e43c69e536a903c37c6cf20749c1b
LAPTOP-638SSQ21\banan <t108590046@ntut.org.tw>
2021-05-03 21:47:28 +0800

    hw8

---
+++ 108590046_HW8/05.1/05.1/app/src/main/java/com/example/a5_1/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.a5_1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+    private int level =0;
+    private ImageView image;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        image = findViewById(R.id.battery);
+        image.setImageResource(R.drawable.levellist);
+    }
+    public void add(View view){
+        if(level<6){
+            level++;
+        }
+        image.setImageLevel(level);
+    }
+    public void minus(View view){
+        if(level>0){
+            level--;
+        }
+        image.setImageLevel(level);
+    }
+}
